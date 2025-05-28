482d76e3bc3d7a99f07e52d76bd3e869b1663743
https-dre <diaso.andre@outlook.com>
2025-05-28 10:37:59 -0300

    feat: add functionality for building and managing containers
    - implementation of the buildImage function to package and build Docker images
    - creation of the createContainer function to generate containers from existing images
    - addition of the startContainer function to start created containers
    - use of context to manage asynchronous operations with Docker

--- core/build.go
+++ core/build.go
@@ -3,11 +3,17 @@ package core
 import (
 	"archive/tar"
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
+	"motion/pkgs/models"
 	"os"
 	"path/filepath"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
 )
 
 // tarProjectContext cria um tarball da pasta do projeto (Dockerfile incluso)
@@ -90,4 +96,77 @@ func extractImageId(buildOutput io.Reader) (string, error) {
 	}
 
 	return "", fmt.Errorf("erro ao extrair ID da imagem")
+}
+
+func (c *Instance) buildImage(ctx context.Context, projectPath, imageName string) (string, error) {
+	cli := c.Client
+	// Empacota a pasta do projeto como contexto
+	tarContext, err := tarProjectContext(projectPath)
+	fmt.Printf("Packing %s\n", imageName)
+
+	if err != nil {
+		return "", fmt.Errorf("erro ao criar tar do contexto: %w", err)
+	}
+
+	// builda a imagem
+	fmt.Println("Creating Image...")
+	buildResp, err := cli.ImageBuild(ctx, tarContext, types.ImageBuildOptions{
+		Tags:       []string{imageName},
+		Remove:     true,
+		Dockerfile: "Dockerfile", // padrão
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("erro ao buildar imagem: %w", err)
+	}
+
+	defer buildResp.Body.Close()
+
+	imageID, err := extractImageId(buildResp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("Image created [ID = %s]\n", imageID)
+
+	return imageID, nil
+}
+
+func (c *Instance) createContainer(ctx context.Context, service *models.CoreApplication) error {
+	cli := c.Client
+
+	imageList, err := cli.ImageList(ctx, image.ListOptions{})
+
+	if err != nil {
+		return err
+	}
+
+	for _, image := range imageList {
+		if image.ID == service.ImageId {
+			containerResponse, err := cli.ContainerCreate(ctx, &container.Config{
+				Image: service.ImageId,
+				Env:   service.Env,
+			}, nil, nil, nil, service.Name)
+
+			if err != nil {
+				return err
+			}
+
+			service.CoreId = containerResponse.ID
+
+			return nil
+		}
+	}
+
+	return nil
+}
+
+func (c *Instance) startContainer(ctx context.Context, containerID string) error {
+	cli := c.Client
+
+	if err := cli.ContainerStart(ctx, containerID, container.StartOptions{}); err != nil {
+		return fmt.Errorf("failed to start container with id %s, %v", containerID, err)
+	}
+
+	return nil
 }
